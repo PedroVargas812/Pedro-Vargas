c688b8284c126fc4acb7a372d53188bdbba66dd0
PedroVargas812 <pedro.vargas@udla.edu.ec>
2025-05-01 16:38:49 -0500

    programa de mascotas

--- src/especies.java
+++ src/especies.java
@@ -1,3 +1,29 @@
 public class especies {
+public enum Tipos{
+  Blanca, BoaConstrictor, Titidorado, guacamayaroja }
+ private String nombre;
+ private int anios;
+ private String clase;
+ private int precio;
+ private int cantidad;
+ private int ventas;
+
+
+ void establecernombre(String nombre){
+ this.nombre = nombre;
+ }
+ void estableceranios(int anios){
+     this.anios= anios;
+    }
+
+    void establecerTipo(String clase){
+        this.clase=clase;
+    }
+    void establecerTipo(int precio){
+        this.precio=precio;
+        }
+
+
+
 
 }
